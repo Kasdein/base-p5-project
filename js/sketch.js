fb59a5b9fe51135f0f74fc6bc9faa9f2e59f7059
Kasdein <kasdein@outlook.fr>
2023-01-10 11:56:00 +0000

    animated sinewave

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,29 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight)
-
     //variables
     let x = 0
     let y = 0
     let theta = 0
-    let amplitude = windowHeight*0.025
-    let frequency = windowWidth*0.01
-    let maxAngle = (windowWidth/frequency) * TAU
+    let inc = 0.05
+    let offset = 0 //the ammount offset from beginning of wave
+    let amplitude
+    let frequency
+    let maxAngle
 
-    //creates a sine wave
-    while(theta < maxAngle){
-        y = sin(theta) * amplitude
-        ellipse(x, y + height*0.5, 25)
-        theta += 0.1
-        x = (theta/maxAngle) * windowWidth
-    }
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+
+    // amplitude = windowHeight*0.25
+    // frequency = windowWidth*0.001
+
+
+    // amplitude = windowHeight * 0.042
+    // frequency = windowWidth * 32
+
+
+    amplitude = windowHeight * 0.25
+    frequency = windowWidth * 2
+    maxAngle = (windowWidth/frequency) * TAU
+
 
 
 
@@ -26,8 +34,42 @@ function setup() {
 
 function draw() {   
 
+    // fill(0, 153, 0)
 
 
+    background(0)
+    noStroke()
+
+    while(theta < maxAngle + offset) {
+    //creates a sine wave
+    // sine 2
+        y = sin(theta) * amplitude
+        fill(204, 255, 255)
+        ellipse(x, y + height * 0.5, 25)
+        theta += 0.1
+        x = ((theta - offset)/maxAngle * windowWidth)   
+    //sine 2
+        y = sin(theta) * amplitude
+        fill (255, 204, 153)
+        ellipse(x, y + height * 0.5, 25)
+        theta += 0.1
+        x = ((theta - offset)/maxAngle * windowWidth)
+
+    //creates a cos wave
+    // cos 1
+        y = cos(theta) * amplitude
+        ellipse(x, y + height * 0.5, 25)
+        theta += 0.1
+        x = ((theta - offset)/maxAngle * windowWidth)
+    //cos 2
+        y = cos(theta) * amplitude
+        fill(204, 255, 255)
+        ellipse(x, y + height * 0.5, 25)
+        theta += 0.1
+        x = ((theta - offset)/maxAngle * windowWidth)
+    }
+    offset += inc
+    theta = offset
 
     // background(250)
 
