ca2d26eee0052143406f8b87c1d3e74679f18cf6
Kasdein <kasdein@outlook.fr>
2023-03-08 18:06:33 +0000

    overhauled random and interaction

--- js/interaction.js
+++ js/interaction.js
@@ -21,7 +21,6 @@ function setup() {
     createCanvas(windowWidth, windowHeight)
     background(0)
     // frameRate(20)
-    amplitude = windowHeight * 0.1
     frequency = windowWidth * 6
     maxAngle = (windowWidth/frequency) * TAU
 
@@ -37,7 +36,7 @@ for (let i = 0; i < num; i++) {
 function draw() {
     // cursor 
     background(0)
-    fill(77, 153, 250)
+
         variableEllipse(mouseX, mouseY, pmouseX, pmouseY)
     }
 
@@ -46,15 +45,11 @@ function variableEllipse(x, y, px, py) {
     let length = 5
     while(theta < maxAngle + offset) {
             for (i = length; i > 0; i --){
-                // y = mouseY + (cos(theta) * amplitude)
-                r = height * 0.12
+                r = height * 0.08
                 x = mouseX + (r * cos(theta))
                 y = mouseY + (r * sin(theta))
-                // fill (255 - (255/length * i), 0, 0)
-                // ellipse(x, y, 12, 12)
                 px = mouseX + (r * sin(theta))
                 py = mouseY + (r * cos(theta))
-                let circleColor = color(0)
                 let which = frameCount % num
                 mx[which] = px 
                 my[which] = py
@@ -64,31 +59,16 @@ function variableEllipse(x, y, px, py) {
                 // which+1 is the smallest (the oldest in the array)        
                 let index = (which + 1 + i) % num
                 let index2 = (which + 1 + i) % num
-                // stroke(circleColor)
-                // noStroke()
-                // fill (255 - (255/length * i), 0, 0)
+                noStroke()
+                fill(77, 153, 250)
                 ellipse(mx[index], my[index], i, i)
+                fill(250, 153, 77)
                 ellipse(mx2[index2], my2[index2], i, i)
                 }
             }
-        theta += 0.1
+        theta += 0.02
         x = ((theta)) * windowWidth  
         }
         offset += inc
 }
-
-// function setup() {    
-// cursor    for (let i = 0; i < num; i++) {        
-// mx.push(i);        my.push(i);    }
-// }
-
-// function draw() {    
-// cursor    let which = frameCount % num;    
-// mx[which] = mouseX;    my[which] = mouseY;    
-// for (let i = 0; i < num; i++) {        
-// which+1 is the smallest (the oldest in the array)        
-// let index = (which + 1 + i) % num;        
-// ellipse(mx[index], my[index], i, i);        
-// stroke(circleColor);    }
-// }
 
--- js/random.js
+++ js/random.js
@@ -19,12 +19,12 @@ function setup() {
     // find a random point on a circle
     let angle1 = random(0, 2 * PI);
     let xpos1 = (mouseX + cos(angle1))
-    let ypos1 = ( mouseY + sin(angle1))
+    let ypos1 = (mouseY + sin(angle1))
   
     // find another random point on the circle
     let angle2 = random(0, 2 * PI);
-    let xpos2 = (mouseX + (cos(angle2)* 55))
-    let ypos2 = (mouseY + (sin(angle2)* 55))
+    let xpos2 = (mouseX + (cos(angle2)* 100))
+    let ypos2 = (mouseY + (sin(angle2)* 100))
   
     // draw a line between them
     line(xpos1, ypos1, xpos2, ypos2);
